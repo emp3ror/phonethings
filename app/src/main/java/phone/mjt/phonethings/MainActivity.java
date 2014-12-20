eb7945a4f709c70429c342db5a9a142b1210e16d
emp3ror <thapa.manish16@gmail.com>
2014-12-20 21:08:37 +0545

    about page creation ;)

--- app/src/main/java/phone/mjt/phonethings/MainActivity.java
+++ app/src/main/java/phone/mjt/phonethings/MainActivity.java
@@ -277,7 +277,9 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.about) {
+            Intent about = new Intent(this, About.class);
+            startActivity(about);
             return true;
         }
 
