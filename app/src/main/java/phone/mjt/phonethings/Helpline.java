e27447e468499b7ab3e48632a09f2f68a5311b3e
emp3ror <thapa.manish16@gmail.com>
2014-12-24 21:56:22 +0545

    orientation for flash light and meny fix

--- app/src/main/java/phone/mjt/phonethings/Helpline.java
+++ app/src/main/java/phone/mjt/phonethings/Helpline.java
@@ -232,7 +232,7 @@ public class Helpline extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_helpline, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -244,7 +244,9 @@ public class Helpline extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.about) {
+            Intent about = new Intent(this, About.class);
+            startActivity(about);
             return true;
         }
 
